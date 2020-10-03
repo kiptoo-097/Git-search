ef416488ae9f95a1e70f8aa2adea83720990246e
kiptoo-097 <kiptoo097@gmail.com>
2020-10-03 16:57:46 +0300

    generated git service

--- src/app/git/git.component.ts
+++ src/app/git/git.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-git',
   templateUrl: './git.component.html',
   styleUrls: ['./git.component.css']
 })
 export class GitComponent implements OnInit {
+  user:string;
+  repo:string;
+  username:string;
 
   constructor() { }
 
